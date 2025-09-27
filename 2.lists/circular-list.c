5dbd539cf5549721572ec4faaf4a6c77a470b460
Francisco Sérgio Feitosa <fsfsegundo@hotmail.com>
2025-09-27 18:28:44 -0300

    remade circ list

--- 2.lists/circular-list.c
+++ 2.lists/circular-list.c
@@ -1,96 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node{
+typedef struct node {
     int val;
     struct node *next;
 }NODE;
 typedef NODE *circularList;
 
-void createList(circularList *l){
-    *l=NULL;
-   
+void create (circularList *l){
+    *l=NULL;  //aponta para final da lista
 }
 
-int size (circularList l){
-    if(!l){return 0;}
-    circularList start;
-    start=l->next;
-    int i;
-    for(i=1;start!=l;start=start->next,i++);
+int tam (circularList l){
+    if(!l){return 0;};
+    circularList aux =l->next; //aux recebe elemento 1
+    int i=1;
+    while(aux!=l){
+        aux=aux->next;
+        i++;
+    }
     return i;
 }
+//IMPLEMENTACAO LINDER
+// void ins (circularList *l, int pos, int val){
+//     if (pos<1||pos>tam(*l)+1){printf("out of bounds (ins)");exit(1);}
+//     circularList novo=(NODE*)malloc(sizeof(NODE));
+//     //casos: ins no inicio vazio, ins no final, ins outras pos
+//     novo->val=val;
+//     int size = tam(*l);
+//     if((*l)==NULL){
+//         novo->next=novo;
+//         *l=novo;
+//     }else{
+//         circularList aux=*l;
+//         if(pos==size+1){ //ins novo ultimo elemento
+//             (*l)=novo;
+//         }else
+//         for(;pos>1;aux=aux->next, pos--);
+//         novo->next=aux->next;
+//         aux->next=novo;
+//     }
+// }
 
-void insert (circularList *l, int pos, int value){
-    if (pos<1||pos>size(*l)+1){
-        printf("out of bounds (ins)");exit(1);
-    }
-    circularList new;
-    new=(NODE*)malloc(sizeof(NODE));
-    //hurr duur check if mem was really allocated hurdurrrr
-    new->val=value;
-    if(!(*l)){
-        new->next=new;
-        *l=new;
-    }
-    else{
-        if(pos==size(*l)+1){//insertion on end
-            circularList aux=*l;
-            new->next=aux->next;
-            aux->next=new;
-            *l=new;
-        }else{//insert on "middle"
-            circularList aux=(*l)->next;
-            int i=0;
-            for(;i<pos-2;aux=aux->next,i++);//stops right before inserted node
-            new->next=aux->next;
-            aux->next=new;
-        }
-    }
-}
-
-int recup(circularList l, int pos){
-    if (pos<1||pos>size(l)){
-        printf("out of bounds (rec)");exit(2);
-    }
-    circularList aux=l->next;
-    for(int i=0;i<pos-1;aux=aux->next,pos++);    
-    return aux->val;
-}
-
-void ret(circularList *l,int pos){
-    if (pos<1||pos>size(*l)){
-        printf("out of bounds (ret)");exit(3);
-    }
-    if(size(*l)==1){//if list has only 1 element
-        free(*l);
-        *l=NULL;return;
+void ins (circularList *l, int pos, int val){
+    if (pos<1||pos>tam(*l)+1){printf("out of bounds (ins)");exit(1);}
+    circularList novo=(NODE*)malloc(sizeof(NODE));
+    //casos: ins no inicio vazio, ins no final, ins outras pos
+    novo->val=val;
+    int size = tam(*l);
+    if(!(*l)){ // se lista vazia
+        novo->next=novo;
+        *l=novo;
+    }else if(pos==1){ //inserindo no inicio
+        novo->next=(*l)->next;
+        (*l)->next=novo;
     }else{
-        circularList aux=(*l)->next,rem;
-        int i=0;
-                for(;i<pos-1;aux=aux->next,i++);
-                rem=aux->next;
-                aux->next=rem->next;
-                free(rem);
+        circularList aux=*l;
+        if(pos==tam(*l)+1){ //ins nov ultimo nodo
+            novo->next=aux->next;
+            aux->next=novo;
+            *l=novo;
+        }else{ //inserir demais pos
+            for(;pos>1;aux=aux->next,pos--);
+            novo->next=aux->next;
+            aux->next=novo;
         }
+
     }
-
-
-void destroy(){
-
 }
 
-int main(){
+int recup (circularList l, int pos){
+    if (pos<1||pos>tam(l)){printf("out of bounds (rec)");exit(2);}
+    for(;pos>0;pos--,l=l->next);
+    return l->val;
+}
+
+void rem (circularList *l, int pos){
+    int size =tam (*l);
+    if (pos<1||pos>size){printf("out of bounds (rem)");exit(3);}
+    //remover unico, remover ultimo, else
+    if(size==1){
+        free (*l);
+        *l=NULL;
+    }else{
+        circularList rem, aux=*l;
+        int i = pos;
+        for(;i>1;aux=aux->next,i--);
+        rem=aux->next;
+        aux->next=rem->next;
+        if(size==pos) { 
+            *l=aux;}
+        free(rem);
+    }
+}
+
+void obliterar (circularList l){
+    circularList aux=l->next;
+    while(l){
+        aux=l;
+        l=l->next;
+        free(aux);
+    }
+}
+
+int main (){
     circularList l;
-    createList(&l);
-    insert(&l,1,2);
-    insert(&l,2,4);
-    insert(&l,3,6);
-    printf("%d \n", recup(l,1));
-    printf("%d \n", recup(l,2));
-    printf("%d \n", recup(l,3));
-    ret(&l,1);
-    printf("\n %d \n", recup(l,1));
-    printf("%d \n", recup(l,2));
-    printf("%d \n", recup(l,3));
+    create(&l);
+    ins(&l,1,2);
+    ins(&l,2,4);
+    ins(&l,3,6);
+    obliterar(l);
+    printf("%d , %d", recup(l,2), recup(l,1));
+    // printf("%d", l->val);
 }
